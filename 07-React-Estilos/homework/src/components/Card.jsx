1fb1cd5491333de972188546292bcdd4f0c59505
nakiche <tommynaquiche@hotmail.com>
2022-09-29 23:57:41 -0500

    07-React-stilos funcionando con código base del Homework 06

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({max,min,name,img,onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return ( 
+  <div>
+    <div onClick={onClose}>x</div>
+    <h3>{name}</h3>
+    <h3>Max    Min</h3>
+    <h3>{max} {min}</h3>
+    <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt=""/>
+  </div>
+  )
 };
--- 07-React-Estilos/homework/src/components/Cards.jsx
+++ 07-React-Estilos/homework/src/components/Cards.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
-
-export default function Cards(props) {
+import Card from './Card.jsx';
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  return (
+  
+      
+        cities.map(ciudad => 
+          <Card 
+           max={ciudad.main.temp_max}
+           min={ciudad.main.temp_min}
+           name={ciudad.name}
+           img={ciudad.weather[0].icon}
+           onClose={() => alert(ciudad.name)}
+
+          />
+        )         
+
+
+    ) 
 };
